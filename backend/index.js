6ba13243c00d84345bce4db2366e97ac0221cacc
sotirispanourgias <panourgiassotiris@gmail.com>
2026-03-26 23:52:01 +0200

    db no localhost

--- backend/index.js
+++ backend/index.js
@@ -12,14 +12,13 @@ app.use(express.static(path.join(__dirname, "../frontend/public")));
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",//192.168.1.211
-  port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "impostor_user",
-  password: process.env.DB_PASSWORD || "impostor_pass",
-  database: process.env.DB_NAME || "impostor",
+  host: "192.168.1.211",
+  user: "impostor_user",
+  password: "impostor_pass",
+  database: "impostor",
+  port: 3306,
   waitForConnections: true,
   connectionLimit: 10,
-  queueLimit: 0,
 });
 
 const dbAll = async (sql, params = []) => {
